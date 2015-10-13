d5a5874681b18c7b981f7728a32a90b741a56d5f
Oliver Chang <oliver@oychang.com>
2015-10-12 22:22:41 -0400

    Adapt prober.sh to emit to file in log format.

--- tools/server/prober.sh
+++ tools/server/prober.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-logger '****** Start boot script ****'
+logger 'STARTING PROBE REQUEST CAPTURE'
 
-DATE=$(date +"%Y%m%d%H%M")
-
-# Clean up older files from log directory and archive them
-mv -n /sd/log/*.pcap* /sd/log/archive/ >/dev/null
-sleep 5
-
-# Running tcpdump
-
-logger '***** Start tcp dump *****'
+# Start WiFi monitoring interface
 ifconfig wlan0 up;
 airmon-ng start wlan0;
-tcpdump -U -s0 -i wlan0mon -w /sd/log/device-$DATE.pcap >/dev/null 2>&1 'type mgt subtype probe-req' &
-logger '***** Finished boot script *****'
+
+# Start tcpdump monitoring and save to file
+DATE=$(date +"%Y%m%d%H%M")
+tcpdump -s0 -i wlan0mon -l 'type mgt subtype probe-req' > "$DATE.log" &
+./server.py > /dev/null
+
+logger '=================================='
+logger 'PROBE REQUEST CAPTURE STARTUP DONE'
+logger '=================================='
