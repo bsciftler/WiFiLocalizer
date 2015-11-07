355f0c0b5e510167f59e78bbac024bb00449435e
Oliver Chang <oliver@oychang.com>
2015-11-07 18:21:55 -0500

    Cleanup MapListFragment.

--- app/src/main/java/edu/fiu/mpact/wifilocalizer/MapListFragment.java
+++ app/src/main/java/edu/fiu/mpact/wifilocalizer/MapListFragment.java
@@ -15,31 +15,20 @@ import android.widget.SimpleCursorAdapter;
 
 
 public class MapListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
-
     private static final int LOADER_ID = 1;
     private static final String[] FROM = {Database.Maps.DATA, Database.Maps.NAME};
-    private static final String[] CURSOR_COLUMNS = {Database.Maps.ID, Database.Maps.DATA,
-            Database.Maps.NAME};
+    private static final String[] CURSOR_COLUMNS = {Database.Maps.ID, Database.Maps.DATA, Database.Maps.NAME};
     private static final int[] TO = {R.id.li_map_image, R.id.li_map_name};
 
     private SimpleCursorAdapter mAdapter;
-
-
-    // FIXME isn't this unnecessary?
-    public MapListFragment() {
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        // FIXME reverse the order so the newest sessions are at the top
         mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.map_list_item, null, FROM, TO,
                 0);
         mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
-
-
             @Override
             public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
                 if (view.getId() == R.id.li_map_image) {
@@ -49,12 +38,10 @@ public class MapListFragment extends ListFragment implements LoaderManager.Loade
 
                 return false;
             }
-
         });
         setListAdapter(mAdapter);
 
         getLoaderManager().initLoader(LOADER_ID, null, this);
-
     }
 
     @Override
@@ -62,14 +49,11 @@ public class MapListFragment extends ListFragment implements LoaderManager.Loade
         final Intent nextIntent = new Intent(getActivity(), ViewMapActivity.class);
         nextIntent.putExtra(Utils.Constants.MAP_ID_EXTRA, id);
         startActivity(nextIntent);
-
-
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(getActivity(), DataProvider.MAPS_URI, CURSOR_COLUMNS, null, null,
-                null);
+        return new CursorLoader(getActivity(), DataProvider.MAPS_URI, CURSOR_COLUMNS, null, null, null);
     }
 
     @Override
@@ -85,45 +69,5 @@ public class MapListFragment extends ListFragment implements LoaderManager.Loade
     @Override
     public void onActivityCreated(Bundle savedState) {
         super.onActivityCreated(savedState);
-
-        //registerForContextMenu(getListView());
     }
-
-    //	@Override
-    //	public void onCreateContextMenu(ContextMenu menu, View v,
-    //									ContextMenu.ContextMenuInfo menuInfo) {
-    //		getActivity().getMenuInflater().inflate(R.menu.main_cmenu, menu);
-    //	}
-
-    //	@Override
-    //	public boolean onContextItemSelected(MenuItem item) {
-    //
-    //		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item
-    // .getMenuInfo();
-    //
-    //		switch (item.getItemId()) {
-    //			case R.id.action_selectMap_cmenu:
-    //				final Intent nextIntent = new Intent(getActivity(),
-    //						ViewMapActivity.class);
-    //				nextIntent.putExtra(Utils.Constants.MAP_ID_EXTRA, info.id); // what is
-    // map_id_extra
-    //				startActivity(nextIntent);
-    //				return true;
-    //			case R.id.action_delete_cmenu:
-    //				String[] mSelectionArgs = {info.id + ""};
-    //				getActivity().getApplicationContext().getContentResolver().delete(DataProvider
-    // .MAPS_URI,
-    //						"_id = ?", mSelectionArgs);
-    //				getActivity().getApplicationContext().getContentResolver().delete(DataProvider
-    // .READINGS_URI,
-    //						"map = ?", mSelectionArgs);
-    //				return true;
-    //			default:
-    //				return super.onContextItemSelected(item);
-    //		}
-
-    //	}
-
-
 }
-
